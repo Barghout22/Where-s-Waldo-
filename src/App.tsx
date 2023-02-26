bab247a184ae6c93dbda3ec3f04a90b772c8b5ce
Mahmoud <barghoutmahmoud2@gmail.com>
2023-02-26 03:20:52 +0200

    Created an initial working version of the game. created everything in the same file as an initial version. created states for the characters were ar looking for . the main container is divided into a mesh-like screen of small divs. the indeces of divs with desired characters is stored into the states mentioned earlier as well as a property for if they were found or not. There is a state that stores the values of divs with characters that were found to make them unclickable. there is a state that stores whether any items on the screen are currently clicked on or active or not. there is a list that appears when an available spot is clicked on and it contains the characters that weren't found yet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,169 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+// import wheresLuffy from "./images/wheresLuffy.png";
+import { useState } from "react";
+// const uniqid = require("uniqid");
 function App() {
+  const [clicked, setClicked] = useState(false);
+  const [isActive, setIsActive] = useState(-1);
+  const [previouslyClicked, setPreviouslyClicked] = useState([-1]);
+  const [searchItems, setSearchItems] = useState([
+    { name: "Luffy", block: 58, found: false },
+    { name: "Yamato", block: 303, found: false },
+    { name: "Jinbe", block: 128, found: false },
+    { name: "Hawkins", block: 198, found: false },
+    { name: "Apoo", block: 242, found: false },
+    { name: "Waldo", block: 283, found: false },
+    { name: "Bartolomeo", block: 88, found: false },
+  ]);
+
+  function checkChoice(index: number, name: string) {
+    setClicked(false);
+    let myItems = searchItems;
+    let itemChecked = myItems.find((item) => item.name === name);
+    let posOfItem = myItems.findIndex((item) => item.name === name);
+    if (itemChecked!.block === index) {
+      itemChecked!.found = true;
+      myItems.splice(posOfItem, 1, itemChecked!);
+      setSearchItems(myItems);
+      setPreviouslyClicked([...previouslyClicked, index]);
+    } else {
+    }
+  }
+
+  function displayItem(number: number) {
+    if (previouslyClicked.includes(number)) return;
+    if (!clicked) {
+      setClicked(true);
+      setIsActive(number);
+    } else {
+      setClicked(false);
+      setIsActive(-1);
+    }
+  }
+  let items = [0];
+  for (let i = 1; i < 345; i++) {
+    items.push(i);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      {/* <header>
+        <h1>Find Luffy</h1>
+        <span>Luffy</span>
+        <span>Momonosuke</span>
+        <span>Aokiji</span>
+        <span>Basel Hawkins</span>
+        <span>Scratchmen Apoo</span>
+        <span>Jinbe</span>
+        <span>Waldo</span>
+      </header> 
+       onClick={displayClick}
+       style={myListStyle}
+      */}
+      <div className="main">
+        {items.map((item, index) => (
+          <div key={index}>
+            <div
+              className={
+                isActive === index
+                  ? "containerItem display"
+                  : previouslyClicked.includes(index)
+                  ? "containerItem found"
+                  : "containerItem "
+              }
+              onClick={() => {
+                displayItem(index);
+              }}
+            >
+              {/* {item} */}
+            </div>
+            {isActive === index && (
+              <div>
+                {/* <div className="clickBox" style={mystyle}></div> */}
+                <div className="choiceList">
+                  {!searchItems[0].found && (
+                    <span
+                      className="options "
+                      onClick={() => {
+                        checkChoice(index, "Luffy");
+                      }}
+                    >
+                      Luffy
+                    </span>
+                  )}
+                  {!searchItems[1].found && (
+                    <span
+                      className="options "
+                      onClick={() => {
+                        checkChoice(index, "Yamato");
+                      }}
+                    >
+                      Yamato
+                    </span>
+                  )}
+                  {!searchItems[2].found && (
+                    <span
+                      className="options "
+                      onClick={() => {
+                        checkChoice(index, "Jinbe");
+                      }}
+                    >
+                      Jinbe
+                    </span>
+                  )}
+                  {!searchItems[3].found && (
+                    <span
+                      className="options"
+                      onClick={() => {
+                        checkChoice(index, "Hawkins");
+                      }}
+                    >
+                      Basel Hawkins
+                    </span>
+                  )}
+                  {!searchItems[4].found && (
+                    <span
+                      className="options"
+                      onClick={() => {
+                        checkChoice(index, "Apoo");
+                      }}
+                    >
+                      Scratchmen Apoo
+                    </span>
+                  )}
+                  {!searchItems[5].found && (
+                    <span
+                      className="options "
+                      onClick={() => {
+                        checkChoice(index, "Waldo");
+                      }}
+                    >
+                      Waldo
+                    </span>
+                  )}
+                  {!searchItems[6].found && (
+                    <span
+                      className="options "
+                      onClick={() => {
+                        checkChoice(index, "Bartolomeo");
+                      }}
+                    >
+                      Bartolomeo
+                    </span>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      {/* <div className="footer">
+        image credit goes to{" "}
+        <a href="https://www.reddit.com/r/OnePiece/comments/olbv3f/heres_the_collab_between_wheres_wally_and_op/">
+          This post on reddit
         </a>
-      </header>
+      </div> */}
     </div>
   );
 }
